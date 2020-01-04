79083edb64c8b0ee8420ec3a03f8e8f8a735428b
kaylasf <sanfilippokayla@gmail.com>
2020-01-04 15:09:38 -0800

    still working

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,40 @@ var dateDisplay = moment(now).format('LL')
 console.log(hour)
 
 var time = $('.time')
-console.log(time)
+
+for( var i = 0 ; i <= time.length -1; i++){
+
+    if(time[i].innerHTML === hour){
+        var current = time[i].innerHTML
+        $(`.${current}`).addClass('present')
+    }
+
+     if(time[i].innerHTML < hour){
+        console.log(time[i])
+        $(`.${time[i].innerHTML}`).addClass('past')
+    }
+    if(time[i].innerHTML > hour) {
+        console.log("hi")
+        $(`.${time[i].innerHTML}`).addClass('past')
+    }
+
+
+}
+
+
+
+
 
 $('#currentDay').append(dateDisplay)
+
+//document on load- display notes thatre saved in local storage
+
+// on click , save notes to local storage 
+
+
+$( ".save" ).click(function() {
+console.log("hi")
+
+
+});
+
